ef58f353919bde1c2e536357c2e2a7df844640e4
jpe7s <jpe7s.salt188@passfwd.com>
2024-10-01 14:55:25 -0500

    skip timing tests on virtual servers.

--- core/src/test/java/software/sava/services/core/remote/call/CallTests.java
+++ core/src/test/java/software/sava/services/core/remote/call/CallTests.java
@@ -1,6 +1,10 @@
 package software.sava.services.core.remote.call;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ConditionEvaluationResult;
+import org.junit.jupiter.api.extension.ExecutionCondition;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import software.sava.services.core.remote.load_balance.BalancedItem;
 import software.sava.services.core.remote.load_balance.LoadBalancer;
 import software.sava.services.core.request_capacity.CapacityConfig;
@@ -11,6 +15,8 @@ import software.sava.services.core.request_capacity.trackers.ErrorTrackerFactory
 import software.sava.services.core.request_capacity.trackers.RootErrorTracker;
 import systems.comodal.jsoniter.JsonIterator;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicLong;
@@ -18,8 +24,6 @@ import java.util.concurrent.atomic.AtomicLong;
 import static org.junit.jupiter.api.Assertions.*;
 
 final class CallTests {
-
-  private static final long NUM_CPUS = Runtime.getRuntime().availableProcessors();
 
   private static final class LongErrorTracker extends RootErrorTracker<Long> {
 
@@ -49,17 +53,15 @@ final class CallTests {
 
     @Override
     protected void logResponse(final Long response) {
-      if (NUM_CPUS > 2) {
-        if (response == 400) {
-          assertTrue(capacityState.capacity() <= 1,
-              response + ": " + capacityState);
-        } else if (response == 401) {
-          assertEquals(398, capacityState.capacity(),
-              response + ": " + capacityState);
-        } else if (response == 429) {
-          assertTrue(capacityState.capacity() > -100 && capacityState.capacity() < 0,
-              response + ": " + capacityState);
-        }
+      if (response == 400) {
+        assertTrue(capacityState.capacity() <= 1,
+            response + ": " + capacityState);
+      } else if (response == 401) {
+        assertEquals(398, capacityState.capacity(),
+            response + ": " + capacityState);
+      } else if (response == 429) {
+        assertTrue(capacityState.capacity() > -100 && capacityState.capacity() < 0,
+            response + ": " + capacityState);
       }
     }
   }
@@ -74,6 +76,25 @@ final class CallTests {
     }
   }
 
+  public static class SkipOnVirtualServersCondition implements ExecutionCondition {
+
+    private static final String VIRTUAL_SERVER = System.getenv("VIRTUAL_SERVER");
+
+    @Override
+    public ConditionEvaluationResult evaluateExecutionCondition(final ExtensionContext context) {
+      return VIRTUAL_SERVER == null
+          ? ConditionEvaluationResult.enabled("Not a virtual server.")
+          : ConditionEvaluationResult.disabled(VIRTUAL_SERVER);
+    }
+  }
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @ExtendWith(SkipOnVirtualServersCondition.class)
+  public @interface SkipOnVirtualServers {
+
+  }
+
+  @SkipOnVirtualServers
   @Test
   void testCourteous() {
     final var serviceName = "testCall";
@@ -109,7 +130,6 @@ final class CallTests {
     int s = 0;
     final long[] samples = new long[900];
     Arrays.fill(samples, -1);
-    Runtime.getRuntime().gc();
     for (int i = 1; i <= samples.length; ++i) {
       final long start = System.currentTimeMillis();
       final var callCount = call.get();
@@ -121,16 +141,16 @@ final class CallTests {
             i, callCount, duration);
         assertEquals(402, callCount, log);
         assertTrue(duration >= 3_000, log);
-        assertTrue(duration < (NUM_CPUS > 2 ? 3_100 : 3_400), log);
+        assertTrue(duration < 3_300, log);
       } else if (i == 427) {
         final var log = String.format(
             "[iteration=%d] [callCount=%d] [duration=%dms]%n",
             i, callCount, duration);
         assertEquals(430, callCount, log);
         assertTrue(duration >= 1_000, log);
-        assertTrue(duration < (NUM_CPUS > 2 ? 1_100 : 1_400), log);
+        assertTrue(duration < 1_300, log);
       } else {
-        if (NUM_CPUS > 2 && duration > 34) {
+        if (duration > 34) {
           fail(String.format(
               "[iteration=%d] [callCount=%d] [duration=%dms]%n",
               i, callCount, duration
@@ -141,10 +161,8 @@ final class CallTests {
     }
     final long median = samples[samples.length >> 1];
     assertEquals(0, median, Long.toString(median));
-    if (NUM_CPUS > 2) {
-      final var stats = Arrays.stream(samples).filter(sample -> sample >= 0)
-          .summaryStatistics();
-      assertTrue(stats.getAverage() < 3, stats.toString());
-    }
+    final var stats = Arrays.stream(samples).filter(sample -> sample >= 0)
+        .summaryStatistics();
+    assertTrue(stats.getAverage() < 3, stats.toString());
   }
 }
