ba7400cf876e33725824dffa95db658b19244a58
jpe7s <jpe7s.salt188@passfwd.com>
2024-10-16 19:06:30 -0500

    expand net config

--- core/src/main/java/software/sava/services/core/config/NetConfig.java
+++ core/src/main/java/software/sava/services/core/config/NetConfig.java
@@ -3,9 +3,22 @@ package software.sava.services.core.config;
 import systems.comodal.jsoniter.FieldBufferPredicate;
 import systems.comodal.jsoniter.JsonIterator;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
+
+import static java.util.Objects.requireNonNullElse;
 import static systems.comodal.jsoniter.JsonIterator.fieldEquals;
 
-public record NetConfig(int port) {
+public record NetConfig(String host,
+                        int port,
+                        KeyStore keyStore) {
 
   public static NetConfig parseConfig(final JsonIterator ji) {
     final var parser = new Builder();
@@ -15,19 +28,44 @@ public record NetConfig(int port) {
 
   private static final class Builder implements FieldBufferPredicate {
 
+    private String host;
     private int port;
+    private String keyStoreType;
+    private Path keyStorePath;
+    private char[] password;
 
     private Builder() {
     }
 
     private NetConfig create() {
-      return new NetConfig(port);
+      if (keyStorePath != null) {
+        try (final var is = Files.newInputStream(keyStorePath)) {
+          final var keyStore = KeyStore.getInstance(requireNonNullElse(keyStoreType, "JKS"));
+          keyStore.load(is, password);
+          Arrays.fill(password, (char) 0);
+          return new NetConfig(host, port, keyStore);
+        } catch (final KeyStoreException | NoSuchAlgorithmException | CertificateException e) {
+          throw new RuntimeException(e);
+        } catch (IOException e) {
+          throw new UncheckedIOException(e);
+        }
+      } else {
+        return new NetConfig(host, port, null);
+      }
     }
 
     @Override
     public boolean test(final char[] buf, final int offset, final int len, final JsonIterator ji) {
-      if (fieldEquals("port", buf, offset, len)) {
+      if (fieldEquals("host", buf, offset, len)) {
+        host = ji.readString();
+      } else if (fieldEquals("port", buf, offset, len)) {
         port = ji.readInt();
+      } else if (fieldEquals("keyStoreType", buf, offset, len)) {
+        keyStoreType = ji.readString();
+      } else if (fieldEquals("keyStorePath", buf, offset, len)) {
+        keyStorePath = Path.of(ji.readString()).toAbsolutePath();
+      } else if (fieldEquals("password", buf, offset, len)) {
+        password = ji.applyChars((b, o, l) -> Arrays.copyOfRange(b, o, o + l));
       } else {
         ji.skip();
       }
