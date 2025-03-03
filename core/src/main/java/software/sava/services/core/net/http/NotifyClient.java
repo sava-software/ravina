ed974c581de27e94aa68635b59c04973259d5018
jpe7s <jpe7s.salt188@passfwd.com>
2025-03-03 09:20:36 -0600

    Add default no op if not webhook clients are configured.

--- core/src/main/java/software/sava/services/core/net/http/NotifyClient.java
+++ core/src/main/java/software/sava/services/core/net/http/NotifyClient.java
@@ -3,18 +3,23 @@ package software.sava.services.core.net.http;
 import software.sava.services.core.remote.call.ClientCaller;
 import software.sava.services.core.request_capacity.context.CallContext;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 
+@FunctionalInterface
 public interface NotifyClient {
 
   static NotifyClient createClient(final ExecutorService executorService,
                                    final List<ClientCaller<WebHookClient>> webHookClients,
                                    final CallContext callContext) {
-    return new NotifyClientImpl(executorService, webHookClients, callContext);
+    if (webHookClients.isEmpty()) {
+      final var noop = List.<CompletableFuture<String>>of();
+      return _ -> noop;
+    } else {
+      return new NotifyClientImpl(executorService, webHookClients, callContext);
+    }
   }
 
-  ArrayList<CompletableFuture<String>> postMsg(final String msg);
+  List<CompletableFuture<String>> postMsg(final String msg);
 }
